c971dc69ebe8ac870e710089f46c925eb46dcd92
Pixel <raikinmuj3@gmail.com>
2026-06-30 13:09:47 +0300

    Add files via upload

--- render.js
+++ render.js
@@ -89,12 +89,6 @@ const MONSTER_SPRITES = {
     idle: { img: spriteMonsterIdle, frames: 5, fw: 96, fh: 96 },
     oy: 0, oh: 90,
   },
-  orcdemon:  {
-    run:  { img: spriteMonster,     frames: 6,  fw: 96, fh: 96 },
-    atk:  { img: spriteMonsterAtk,  frames: 4,  fw: 96, fh: 96 },
-    idle: { img: spriteMonsterIdle, frames: 5,  fw: 96, fh: 96 },
-    oy: 0, oh: 90,
-  },
 };
 
 // ── Глобальные переменные рендера ──
